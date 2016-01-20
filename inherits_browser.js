be790435b583f57a98429e9789a3b990987fabc0
Sebastian Krzyszkowiak <s.krzyszkowiak@cogision.com>
2016-01-20 16:49:49 +0100

    add Object.create sanity checking

--- inherits_browser.js
+++ inherits_browser.js
@@ -1,7 +1,13 @@
+var objectCreateSupported = false;
 if (typeof Object.create === 'function') {
+  var tmpObj = Object.create({}, { param: { value: 'test' } });
+  objectCreateSupported = (tmpObj.param === 'test');
+}
+
+if (objectCreateSupported) {
   // implementation from standard node.js 'util' module
   module.exports = function inherits(ctor, superCtor) {
-    ctor.super_ = superCtor
+    ctor.super_ = superCtor;
     ctor.prototype = Object.create(superCtor.prototype, {
       constructor: {
         value: ctor,
@@ -14,10 +20,10 @@ if (typeof Object.create === 'function') {
 } else {
   // old school shim for old browsers
   module.exports = function inherits(ctor, superCtor) {
-    ctor.super_ = superCtor
-    var TempCtor = function () {}
-    TempCtor.prototype = superCtor.prototype
-    ctor.prototype = new TempCtor()
-    ctor.prototype.constructor = ctor
+    ctor.super_ = superCtor;
+    var TempCtor = function () {};
+    TempCtor.prototype = superCtor.prototype;
+    ctor.prototype = new TempCtor();
+    ctor.prototype.constructor = ctor;
   }
 }
